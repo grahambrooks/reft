8a37f871e53c0b96aa3b5e46a6c819cbc0c483f0
Graham Brooks <graham@grahambrooks.com>
2014-04-26 18:50:51 -0400

    Fixed a dependency issue

--- test/java/reft/migration/MigrationReaderTests.java
+++ test/java/reft/migration/MigrationReaderTests.java
@@ -15,7 +15,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.isA;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MigrationReaderTests {
